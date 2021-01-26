6236dd6786980942fedd1b06ec1d9a0070f0f721
Shrivalli Maheshwaran <shrivalli04@gmail.com>
2021-01-26 18:31:11 +0530

    Data modified1

--- Hello.java
+++ Hello.java
@@ -6,7 +6,7 @@ for(int i=1;i<10;i++)
 {
 System.out.println("Modified Data"+i);
 System.out.println("Git is modified");
-System.out.println("Jenkins Added");
+System.out.println("Jenkins Modified");
 }
 }
 }
