01a307e5a35abe9e8e9905bb1e8697d6ad72708e
Shrivalli <shrivalli04@gmail.com>
2021-04-08 09:47:20 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public static void main(String[] args)
 {
 for(int i=1;i<10;i++)
 {
-  System.out.println("welcome");
+  System.out.println("new");
 System.out.println("Modified Data"+i);
 //System.out.println("Git is modified");
 System.out.println("Jenkins Modified");
