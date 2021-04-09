27cc495582cc05aa5ca34663eac49c5604b1253a
Shrivalli <shrivalli04@gmail.com>
2021-04-09 07:48:41 +0530

    Pipeline sample

--- Hello.java
+++ Hello.java
@@ -5,6 +5,7 @@ public static void main(String[] args)
 for(int i=1;i<10;i++)
 {
   System.out.println("new");
+  System.out.println("Pipeline Jenkins");
 System.out.println("Modified Data"+i);
 //System.out.println("Git is modified");
 //System.out.println("Jenkins Modified");
